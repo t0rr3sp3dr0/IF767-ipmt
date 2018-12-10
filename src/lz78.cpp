4ee23feb0b795ac2d8ce76d39a868ab64466d006
Pedro Tôrres <t0rr3sp3dr0@gmail.com>
2018-12-09 21:12:45 -0300

    fix: LZ78

--- src/lz78.cpp
+++ src/lz78.cpp
@@ -1,6 +1,7 @@
 #include "lz78.h"
 
 #include <cmath>
+#include <climits>
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -8,96 +9,101 @@
 const int ALPHABET_SIZE = 256;
 
 struct node {
-    node* children[ALPHABET_SIZE]; 
+    node* children[ALPHABET_SIZE];
     int index;
-    int cur_trie; 
+    int cur_trie;
 
-    
     node(int cur_trie){
-        //memset()
         std::memset(this->children, 0, sizeof(this->children));
+        //std::fill(children,children+ALPHABET_SIZE, nullptr);
         this->index = -1;
         this->cur_trie = cur_trie;
-        //fill(children,children+ALPHABET_SIZE, nullptr);
+        //
     }
-}; 
+};
 
-node* root; 
+node* root = new node(0);
 
 
 
 inline void insert(const std::string &key, int idx, int cur_trie) {
+    //std::cout<<"iniciando o root";
+    //std::cout<<"iniciando o root";
     auto aux = root;
-    int index; 
+    int index;
     for (auto ch : key) {
-        index = ch%ALPHABET_SIZE;
+        index = ch - CHAR_MIN;
         if (aux->children[index] == nullptr) {
             aux->children[index] = new node(cur_trie);
         }else if(cur_trie != aux->children[index]->cur_trie){
-            aux->children[index]->cur_trie = cur_trie; 
+            aux->children[index]->cur_trie = cur_trie;
         }
         aux = aux->children[index];
     }
-    aux->index = idx; 
-    aux->cur_trie = cur_trie; 
+    aux->index = idx;
+    aux->cur_trie = cur_trie;
 }
 inline int search(const std::string &key, int cur_trie) {
     auto aux = root;
-    int index; 
-    int i = 1; 
-    int last_index = 0; 
+    int index;
+    int i = 1;
+    int last_index = 0;
     for (auto ch : key) {
-        index = ch%ALPHABET_SIZE;
+        index = ch - CHAR_MIN;//%ALPHABET_SIZE;
+        //std::cout<<ch<<" "<<index<<std::endl;
         if (aux->children[index] == nullptr || aux->children[index]->cur_trie != cur_trie) {
             return -1;
         }
         aux = aux->children[index];
-        last_index = aux->index; 
+        last_index = aux->index;
     }
     return last_index;
-} 
+}
 
-    //static inline void compress(const ::string_view &txt, std::ostream &out) {
+//static inline void compress(const ::string_view &txt, std::ostream &out) {
 
 void lz78::compress (const ::string_view &in,std::ostream &out){
+    //std::cout<<"start compressing"<<std::endl;
 
     char char0 = 0;
-    int cur_trie = 0; 
-    int vec_size = 0; 
-    int dict_index = 1;
-    int code = 0; 
-    std::string prefix = ""; 
-    int size = 16; 
-    int index_max = pow(2, size); 
-    int last_index = 0; 
-    int idx = 0; 
-    
 
-    insert("", 0, cur_trie); 
-    
+    int cur_trie = 0;
+    int vec_size = 0;
+    int dict_index = 1;
+
+    int code = 0;
+    std::string prefix = "";
+
+    int size = 16;
+    int index_max = pow(2, size);
+    int last_index = 0;
+    int idx = 0;
+
+    insert("", 0, cur_trie);
+
 
     for (auto &c: in){
-
+        //std::cout<<"start compressing";
         if(dict_index == index_max){
-            dict_index = 1;                 
-            cur_trie++; 
-            insert("", 0, cur_trie); 
+            dict_index = 1;
+            cur_trie++;
+            insert("", 0, cur_trie);
         }
 
-        idx = search(prefix+c, cur_trie); 
-
+        idx = search(prefix+c, cur_trie);
+        //std::cout<<idx<<std::endl;
         if(idx > 0){
-            prefix += c;   
-            last_index = idx; 
+            prefix += c;
+            last_index = idx;
         }else{
             if(prefix.empty()){
-                code = 0; 
+                code = 0;
             }else{
                 code = last_index;
             }
 
             insert(prefix+c, dict_index, cur_trie);
-            
+
             if(size == 16){
                 if (code <= 255) {
                     out.write((char*)&char0,sizeof(char));
@@ -110,15 +116,15 @@ void lz78::compress (const ::string_view &in,std::ostream &out){
 
                 }
                 out.write((char*)&c,sizeof(char));
-            }  
+            }
 
-            dict_index++; 
+            dict_index++;
             prefix="";
         }
     }
     if(!prefix.empty()){
-        code = search(prefix, cur_trie); 
-        
+        code = search(prefix, cur_trie);
+
         if (code <= 255) {
             out.write((char*)&char0,sizeof(char));
             out.write((char*)&code,sizeof(char));
@@ -138,30 +144,31 @@ void lz78::decompress (std::istream &file, ::string_view &txt){
     //std::ofstream outfile ("new.txt");
     //std::fstream file ("exemple", std::ios::in | std::ios::out | std::ios::binary);
     int size = 16;
-    std::string dict [65536]; 
+    int index_max = pow(2, size);
+    std::string dict [index_max];
     int dict_index = 1;
-    int aux, aux2; 
-    int num = 0; 
-    std::string aux_text = ""; 
-    int index_max = pow(2, size); 
-    char text_in_num1, text_in_num2, text_in_char;
-    std::string carc=""; 
-    char *_txt = const_cast<char *>(txt.begin());
-    std::stringstream cc; 
+    int aux, aux2;
+    int num = 0;
+    std::string aux_text = "";
 
+    char text_in_num1, text_in_num2, text_in_char;
+    std::string carc="";
+    char *_txt = const_cast<char *>(txt.begin());
+    std::stringstream cc;
+    int i = 0;
     while(!file.eof()){
 
-        file.read((char*)&text_in_num1, sizeof(char)); 
+        file.read((char*)&text_in_num1, sizeof(char));
         file.read((char*)&text_in_num2, sizeof(char));
-        
-        aux = ( 255 & int(text_in_num1));  
+
+        aux = ( 255 & int(text_in_num1));
         aux <<=8;
-	    num |= aux;
-	    aux2 = ( 255 &  int(text_in_num2));
-	    num = aux | aux2;
-        
-        if(num == 0) aux_text = ""; 
-        else aux_text = dict[num]; 
+        num |= aux;
+        aux2 = ( 255 &  int(text_in_num2));
+        num = aux | aux2;
+
+        if(num == 0) aux_text = "";
+        else aux_text = dict[num];
 
         if(file.eof())
             break;
@@ -169,16 +176,14 @@ void lz78::decompress (std::istream &file, ::string_view &txt){
         file.read((char*)&text_in_char, sizeof(char));
         carc =  aux_text+(char)text_in_char;
         dict[dict_index] = carc;
-        aux_text.append(carc); 
 
-        cc<<carc;  
+        cc<<carc;
 
         dict_index++;
         text_in_char = '\0';
         if(dict_index == index_max) dict_index = 1;
     }
+    std::cout<<cc.str();
     txt = cc.str();
-    
     //outfile.close();
 }
-
